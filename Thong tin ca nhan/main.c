6703619209183c1b5b0683ba90cf672ddb383db0
NguyenDoanHung05 <144453577+NguyenDoanHung05@users.noreply.github.com>
2023-09-08 22:30:11 +0700

    Add files via upload

---
+++ Thong tin ca nhan/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char str1[30]= "Nguyen Doan Hung";
+	char str2[11]="0393440817";
+	char str3[30]="hung.nd.2368@aptechlearning.edu.vn";
+	
+	printf("Ten: %s% \nSDT: %s% \nEmail: %s%", str1, str2, str3);
+	return 0;
+}
